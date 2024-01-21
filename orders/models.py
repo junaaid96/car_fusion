7ea33b3e9182b5f69c0592734ed0f45933c1d3ba
junaaid96 <junaaid96@gmail.com>
2024-01-21 21:17:13 +0600

    updates v1.7

--- orders/models.py
+++ orders/models.py
@@ -10,7 +10,7 @@ class Order(models.Model):
     order_date = models.DateTimeField(auto_now_add=True)
     quantity = models.IntegerField(default=1)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=100, default='Pending')
+    status = models.CharField(max_length=100, default='Available')
 
     def __str__(self):
         return f"Order {self.id} by {self.customer.username}"
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
+from .models import Car, Order
 
-# Create your views here.
-def place_order(request):
-    return render(request, 'place_order.html')
+
+@login_required
+def place_order(request, car_id):
+    car = get_object_or_404(Car, id=car_id)
+    user = request.user
+    order = Order.objects.create(
+        customer=user,
+        car=car,
+        quantity=1,
+        total_price=car.price,
+        status='Sold'
+    )
+    order.save()
+    car.quantity -= 1
+    car.save()
+    return render(request, 'order_details.html', {'order': order})
